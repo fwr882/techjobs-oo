3acbd50e624db8e02a360f2c6e4b60e2b4a37ea9
fwr882 <fahimrahman2000@gmail.com>
2017-12-13 00:55:59 -0600

    final did all 6 todos

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,14 +1,21 @@
 package org.launchcode.controllers;
 
+
+import org.launchcode.models.*;
+
+import org.launchcode.models.data.JobDataImporter;
+import org.launchcode.models.data.JobFieldData;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 /**
  * Created by LaunchCode
@@ -24,7 +31,8 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        Job newJob=jobData.findById(id);
+        model.addAttribute("job", newJob);
         return "job-detail";
     }
 
@@ -40,8 +48,25 @@ public class JobController {
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        if(errors.hasErrors()){
+            return "new-job";
+        }
 
-        return "";
+        String jobName = jobForm.getName();
+        Employer jobEmp = jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location jobLoc = jobData.getLocations().findById(jobForm.getLocationId());
+        PositionType jobPosition = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+        CoreCompetency jobCore = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId());
+
+        Job newJob = new Job(jobName,jobEmp,jobLoc,jobPosition,jobCore);
+        jobData.add(newJob);
+        String id = String.valueOf(newJob.getId());
+
+
+
+        return "redirect:?id=" + id;
 
     }
+
+
 }
